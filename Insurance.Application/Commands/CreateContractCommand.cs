c033ad25e97b33abf65cb6574cde440b12c6665c
adrienvadiavaloo <adrienvadiavaloo@gmail.com>
2025-03-11 23:22:50 -0300

    create contact update

--- Insurance.Application/Commands/CreateContractCommand.cs
+++ Insurance.Application/Commands/CreateContractCommand.cs
@@ -11,10 +11,12 @@ namespace Insurance.Application.Commands
     public class CreateContractHandler : IRequestHandler<CreateContractCommand, APIResponse<bool>>
     {
         private readonly ICommandRepository<Contract> _contractCmdRepo;
+        private readonly IQueryRepository<PolicyType> _policyTypeRepo;
 
-        public CreateContractHandler(ICommandRepository<Contract> contractCmdRepo)
+        public CreateContractHandler(ICommandRepository<Contract> contractCmdRepo, IQueryRepository<PolicyType> policyTypeRepo)
         {
             _contractCmdRepo = contractCmdRepo;
+            _policyTypeRepo = policyTypeRepo;
         }
 
         public async Task<APIResponse<bool>> Handle(CreateContractCommand cmd, CancellationToken cancellationToken)
@@ -22,6 +24,15 @@ namespace Insurance.Application.Commands
             try
             {
                 var newContract = Contract.Create(cmd.request.ContractNumber, cmd.request.HolderName, cmd.request.StartDate, cmd.request.EndDate);
+
+                if (cmd.request.PolicyIds.Any())
+                {
+                    // To Add new field Amount for PolicyType 
+
+                    var policies = cmd.request.PolicyIds
+                                                 .Select(policyId => new Policy(newContract.Id, policyId, cmd.request.HolderName, 0))
+                                                 .ToList();
+                }
 
                 var entity = await _contractCmdRepo.CreateAsync(newContract);
 
